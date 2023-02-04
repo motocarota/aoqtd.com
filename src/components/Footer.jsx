4ae2fd9f4c6d00f43dccc67f1a50b750bb9d034e
Motocarota <motocarota@gmail.com>
2023-02-04 09:36:23 +0100

    mv to gatsby
    Former-commit-id: cb41fe18ee2d3e51046e75404f821b273920fe8f

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <div className="footer" style={{ textAlign: 'center' }}>
-      All rights reserved.
-    </div>
-  );
-}
-
-export default Footer;
--- src/utils/queryStringToObject.js
+++
@@ -1,5 +0,0 @@
-// from
-// https://stackoverflow.com/questions/8648892/how-to-convert-url-parameters-to-a-javascript-object
-// converts a query string to an object
-
-export default (str) => Object.fromEntries(new URLSearchParams(str));
