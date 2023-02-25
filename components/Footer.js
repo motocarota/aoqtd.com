45e591cb5e129eaba1a19c8bd4eb16b535808de6
Motocarota <motocarota@gmail.com>
2023-02-25 19:19:57 +0100

    nicer footer
    Former-commit-id: 39f494b20c767d3f83725c6d94195717f4a54520

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,11 @@
+import style from '../cssModules/Footer.module.css';
+
 const Footer = () => {
 
   return (
-    <>
+    <div className={style.footer}>
       <small>All images are Copyright ©2023 <a href="https://motocarota.art" target="_blank" rel="noreferrer">Simone Poggi</a>, All Rights Reserved</small>
-    </>
+    </div>
   );
 };
 
