4875d8c9059d625f177bc6654b22c645e2c52b93
Motocarota <motocarota@gmail.com>
2023-02-25 19:20:01 +0100

    navigation

--- components/Top.js
+++ components/Top.js
@@ -1,10 +1,20 @@
 import Image from "next/image";
+import Link from "next/link";
+import style from "../cssModules/Top.module.css";
 
 const Top = () => {
 
   return (
     <>
-      <Image src="/aoqtd-logo-sm.png" height={237} width={320} alt="www.aoqtd.com logo" />
+      <Link href="/">
+        <Image src="/aoqtd-logo-sm.png" height={237} width={320} alt="www.aoqtd.com logo" />
+      </Link>
+      <nav className={style.header}>
+        <Link href="/">Comic</Link>
+        <Link href="/strip">Strip</Link>
+        <Link href="/rss">RSS</Link>
+        <Link href="/about">About</Link>
+      </nav>
     </>
   );
 };
