02ef7d8429f17d624a645c212752b7244efebfe3
Motocarota <motocarota@gmail.com>
2023-02-25 19:43:06 +0100

    adds some more content

---
+++ pages/monsters.js
@@ -0,0 +1,21 @@
+import path from 'path';
+import fs from 'fs';
+import Gallery from '@/components/Gallery'
+
+export default function Monsters({ pages }) {
+  
+  return (
+    <Gallery pages={pages} comic={'monsters'} width={600} />
+  )
+}
+
+export function getStaticProps() {
+  const postsDirectory = path.join(process.cwd(), './public/monsters');
+  const filenames = fs.readdirSync(postsDirectory);
+
+  return {
+    props: {
+      pages: filenames,
+    },
+  }
+}
