5c90114ad2e85d3330cd06034389184d28eaaa7e
Motocarota <motocarota@gmail.com>
2023-02-26 21:55:19 +0100

    page meta head
    Former-commit-id: 16b051e48ff2eaa50caa960cba92ad76cc81177f [formerly 0e362afc6287bb66d4ecf23b4ed9cca55ac80541]
    Former-commit-id: c9071f3ca8db5a4211d173de65a01c3dc39319b5

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Another One Quest to Dust</title>
+        <meta name="description" content="and yet another stupid fantasy webcomic" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,11 @@
 import path from 'path';
 import fs from 'fs';
-import Head from 'next/head'
 import Gallery from '@/components/Gallery'
 import generateRssFeed from '@/utils/generateFeed';
 
 export default function Home({ pages }) {
   return (
     <>
-      <Head>
-        <title>Another One Quest to Dust</title>
-        <meta name="description" content="and yet another stupid fantasy webcomic" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
       <main>
         <Gallery pages={pages} comic={'comic/en'} width={800} />
       </main>
