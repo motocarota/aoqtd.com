1bb1abda9b77bcb9e6579f2836e5b0c32a0f02e9
Motocarota <motocarota@gmail.com>
2023-10-05 12:45:58 +0200

    mantine setup
    Former-commit-id: 87ebf4aa58144129af610247e74d2abcfcce422a [formerly 22118c93d8596fb0c28add0b8c226695a9bcab57]
    Former-commit-id: 81ce0609f40ff0693d6e2c369d1c8bc062129b0d

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
 import Layout from '@/components/Layout';
+import '@mantine/core/styles.css';
+import {MantineProvider, createTheme} from '@mantine/core';
 
-export default function App({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+const theme = createTheme({
+	/** Put your mantine theme override here */
+	primaryColor: 'orange',
+});
+
+export default function App({Component, pageProps}) {
+	return (
+		<Layout>
+			<MantineProvider theme={theme}>
+				<Component {...pageProps} />
+			</MantineProvider>
+		</Layout>
+	);
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import {Html, Head, Main, NextScript} from 'next/document';
+import {ColorSchemeScript} from '@mantine/core';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <title>Another One Quest to Dust</title>
-        <meta name="description" content="and yet another stupid fantasy webcomic" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang='en'>
+			<Head>
+				<link rel='icon' href='/favicon.ico' />
+				<ColorSchemeScript />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
