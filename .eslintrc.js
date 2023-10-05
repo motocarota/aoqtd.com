0081bc19f134c75d061a57195019e596ef785615
Motocarota <motocarota@gmail.com>
2023-10-05 12:45:00 +0200

    linter fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,23 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'airbnb',
-    'next/core-web-vitals',
-    'plugin:react/recommended',
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-    'react/react-in-jsx-scope': 0,
-    'react/jsx-filename-extension': 0,
-    'react/prop-types': 0,
-    'import/extensions': 0,
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'plugin:react/recommended',
+		'xo',
+	],
+	overrides: [
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: [
+		'react',
+	],
+	rules: {
+		'react/prop-types': 0,
+		'react/react-in-jsx-scope': 0,
+	},
 };
