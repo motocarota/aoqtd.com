9fe2e5c9298d3cf5599a58bdee9e8a0a14c8d2a2
Motocarota <motocarota@gmail.com>
2023-10-07 01:17:09 +0200

    adds skip[ check

--- utils/getChapterLink.js
+++ utils/getChapterLink.js
@@ -1,10 +1,12 @@
 import getImageUrl from './getImageUrl';
 import pad2 from './pad2';
 
-function getChapterLink({pages, chapter}) {
-	const chapterPage = getImageUrl({chapter, page: 0});
-	if (!pages.includes(chapterPage)) {
-		return '';
+function getChapterLink({pages, chapter, skipCheck = false}) {
+	if (!skipCheck) {
+		const chapterPage = getImageUrl({chapter, page: 0});
+		if (!pages.includes(chapterPage)) {
+			return '';
+		}
 	}
 
 	const c = pad2(chapter);
