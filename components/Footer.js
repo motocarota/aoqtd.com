0359d01a3398fc0135e40ae679e68b515f8369ad
Motocarota <motocarota@gmail.com>
2023-10-07 01:17:15 +0200

    mc
    Former-commit-id: 3f4f4c2b303d178d103b4cce26e91069af7badda [formerly c8552bdeb871cb20a4ce3c33000bfd603369e479]
    Former-commit-id: a7a8db90e1e74bef101f3ed25a2fe0541a121f12

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import style from '../styles/Footer.module.css';
 
 const Footer = () => (
 	<div className={style.footer}>
-		<small>All images are Copyright ©2023 <a href='https://motocarota.art' target='_blank' rel='noreferrer'>Simone Poggi</a>, All Rights Reserved</small>
+		<small>Copyright ©2003-2023 <a href='https://motocarota.art' target='_blank' rel='noreferrer'>Simone Poggi</a><br/>All Rights Reserved</small>
 	</div>
 );
 
