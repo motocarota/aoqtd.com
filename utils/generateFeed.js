e8b556735f93a2532f47dbe162fd4480007d9b52
Motocarota <motocarota@gmail.com>
2023-10-07 01:17:42 +0200

    lint

--- utils/generateFeed.js
+++ utils/generateFeed.js
@@ -1,30 +1,31 @@
+/* eslint-disable camelcase */
 import fs from 'fs';
 import RSS from 'rss';
 
 export default async function generateRssFeed(posts) {
- const site_url = 'https://www.aoqtd.com';
+	const site_url = 'https://www.aoqtd.com';
 
- const feedOptions = {
-  title: 'AOQTD.com | RSS Feed',
-  description: 'Another stupid fantasy webcomic!',
-  site_url,
-  feed_url: `${site_url}/rss.xml`,
-  image_url: `${site_url}/aoqtd-logo-sm.webp`,
-  pubDate: new Date(),
-  copyright: `All rights reserved ${new Date().getFullYear()}, Motocarota`,
- };
+	const feedOptions = {
+		title: 'AOQTD.com | RSS Feed',
+		description: 'Another stupid fantasy webcomic!',
+		site_url,
+		feed_url: `${site_url}/rss.xml`,
+		image_url: `${site_url}/aoqtd-logo-sm.webp`,
+		pubDate: new Date(),
+		copyright: `All rights reserved ${new Date().getFullYear()}, Motocarota`,
+	};
 
- const feed = new RSS(feedOptions);
+	const feed = new RSS(feedOptions);
 
- posts.map((post) => {
-  feed.item({
-   title: post,
-   description: `<img src="${post}" alt="${post} />"`,
-   url: `${site_url}/comic/en/${post}`,
-   date: post.date,
-  });
- });
+	posts.map(post =>
+		feed.item({
+			title: post,
+			description: `<img src="${post}" alt="${post} />"`,
+			url: `${site_url}/comic/en/${post}`,
+			date: post.date,
+		}),
+	);
 
- console.log({feed})
- fs.writeFileSync('./public/rss.xml', feed.xml({ indent: true }));
+	console.log({feed});
+	fs.writeFileSync('./public/rss.xml', feed.xml({indent: true}));
 }
