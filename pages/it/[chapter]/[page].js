3eb74940d693496bedfafeaab0cac90f2c2e795a
Motocarota <motocarota@gmail.com>
2023-10-07 15:07:16 +0200

    adds post below comic page
    Former-commit-id: f354ed8bf95828d0954f0c94d6020f3db7544eda [formerly c43745c74d4f56dfbc297703d2b18b0d8f38abf2]
    Former-commit-id: fcc2d01884cb55a95f258c1fe0d9eb3479445e74

--- pages/it/[chapter]/[page].js
+++ pages/it/[chapter]/[page].js
@@ -1,168 +1,44 @@
-import Image from 'next/image';
-import Link from 'next/link';
 import extractValues from '@/utils/extractValues';
-import getPageLink from '@/utils/getPageLink';
-import getChapterLink from '@/utils/getChapterLink';
-import getImageUrl from '@/utils/getImageUrl';
-import {Button, ButtonGroup, Group, Space, Title, UnstyledButton} from '@mantine/core';
-import getLastPage from '@/utils/geLastPage';
-import readAllPages from '@/utils/getAllPages';
-import useKeyboardNavigation from '@/hooks/useKeyboardNavigation';
-import {useRouter} from 'next/router';
+import getComicPage from '@/utils/getComicPage';
+import mdToHtml from '@/utils/mdToHtml';
+import {getPostById} from '@/api/posts';
+import {getAllPages} from '@/api/comics';
+import Post from '@/components/Post';
+import Comic from '@/components/Comic';
 
 export default function Home({
-	imageUrl,
-	page,
-	chapter,
-	prevPage,
-	nextPage,
-	prevChapter,
-	nextChapter,
-	lastPage,
-	isFirstPage,
-	isLastPage,
+	comic,
+	md,
 }) {
-	const router = useRouter();
-
-	useKeyboardNavigation({
-		prev() {
-			if (prevPage) {
-				router.push(prevPage);
-			}
-		},
-		next() {
-			if (nextPage) {
-				router.push(nextPage);
-			}
-		},
-	});
 	return (
 		<>
-			<main>
-				<Title order={3} my='md'>
-          Capitolo:{chapter} Pagina:{page}
-				</Title>
-				<UnstyledButton
-					component={Link}
-					href={nextPage}
-				>
-					<Image
-						width={700}
-						height={500}
-						src={`/comic/it/${imageUrl}`}
-						alt={page}
-					/>
-				</UnstyledButton>
-				<Group justify='center' wrap='wrap'>
-					<ButtonGroup>
-						<Button
-							variant='subtle'
-							disabled={isFirstPage}
-							component={Link}
-							href={'/it/01/00'}
-							leftSection={<Image src='/first.png' width={15} height={15} />}
-						>
-          Inizio
-						</Button>
-						<Button
-							variant='subtle'
-							disabled={!prevChapter}
-							component={Link}
-							href={prevChapter}
-							leftSection={<Image src='/prev-c.png' width={15} height={15} />}
-						>
-          Capitolo
-						</Button>
-
-						<Button
-							variant='subtle'
-							disabled={!prevPage}
-							component={Link}
-							href={prevPage}
-							leftSection={<Image src='/prev.png' width={15} height={15} />}
-						>
-            Pagina
-						</Button>
-					</ButtonGroup>
-
-					<Space px='lg' />
-
-					<ButtonGroup>
-						<Button
-							variant='subtle'
-							disabled={!nextPage}
-							component={Link}
-							href={nextPage}
-							rightSection={<Image src='/next.png' width={15} height={15} />}
-						>
-            Pagina
-						</Button>
-
-						<Button
-							variant='subtle'
-							disabled={!nextChapter}
-							component={Link}
-							href={nextChapter}
-							rightSection={<Image src='/next-c.png' width={15} height={15} />}
-						>
-            Capitolo
-						</Button>
-
-						<Button
-							variant='subtle'
-							disabled={isLastPage}
-							component={Link}
-							href={lastPage}
-							rightSection={<Image src='/last.png' width={15} height={15} />}
-						>
-						Ultima
-						</Button>
-					</ButtonGroup>
-				</Group>
-			</main>
+			<Comic comic={comic} />
+			<Post md={md} />
 		</>
 	);
 }
 
 // Returns all the static data for the page
-export function getStaticProps({params}) {
-	const pages = readAllPages();
+export async function getStaticProps({params}) {
+	// Read post data
+	const post = getPostById(`post-${params.chapter}-${params.page}`);
+	const md = await mdToHtml(post.content || '');
 
-	const imageUrl = getImageUrl(params);
-	const index = pages.indexOf(imageUrl);
-	const {
-		page,
-		chapter,
-	} = params;
-
-	const prevPage = getPageLink({pages, index: index - 1});
-	const nextPage = getPageLink({pages, index: index + 1});
-	const prevChapter = getChapterLink({pages, chapter: parseInt(chapter, 10) - 1});
-	const nextChapter = getChapterLink({pages, chapter: parseInt(chapter, 10) + 1});
-	const last = getLastPage({pages});
-	const lastPage = `/it/${last.chapter}/${last.page}`;
-	const isFirstPage = page === '00' && chapter === '01';
-	const isLastPage = page === last.page && chapter === last.chapter;
+	// Read comic page data
+	const pages = getAllPages();
+	const comic = getComicPage({params, pages});
 
 	return {
 		props: {
-			imageUrl,
-			page,
-			chapter,
-			prevPage,
-			nextPage,
-			prevChapter,
-			nextChapter,
-			lastPage,
-			isFirstPage,
-			isLastPage,
+			comic,
+			md,
 		},
 	};
 }
 
 // Returns all the valid pages from the combination chapter + page dynamic paths
 export function getStaticPaths() {
-	const pages = readAllPages();
+	const pages = getAllPages();
 	const paths = pages.map(
 		fileName => ({
 			params: extractValues(fileName),
