d2f96258450dae826b5c7d3ad397c2ebaff266d0
Motocarota <motocarota@gmail.com>
2023-10-16 00:13:57 +0200

    fix broken feed
    Former-commit-id: a6468df7b5325e6861913d481c26f5586c77be9f [formerly 5698f1e04416fb315c4d7ee2b6ac7b28fb5422dd]
    Former-commit-id: 4c88f7348145a48695b2f2986c3b3fc6d166feff

--- utils/generateFeed.js
+++ utils/generateFeed.js
@@ -1,13 +1,23 @@
 /* eslint-disable camelcase */
 import fs from 'fs';
 import RSS from 'rss';
+import getPageLink from './getPageLink';
+import extractValues from './extractValues';
+
+// Const site_url = 'http://localhost:3000';
+const site_url = 'https://www.aoqtd.com';
+
+function getPostName(post) {
+	const {
+		chapter, page,
+	} = extractValues(post);
+	return `Chapter ${chapter}, Page ${page}`;
+}
 
 export default async function generateRssFeed(posts) {
-	const site_url = 'https://www.aoqtd.com';
-
 	const feedOptions = {
 		title: 'AOQTD.com | RSS Feed',
-		description: 'Another stupid fantasy webcomic!',
+		description: 'Another stupid fantasy webcomic! enjoy!',
 		site_url,
 		feed_url: `${site_url}/rss.xml`,
 		image_url: `${site_url}/aoqtd-logo-sm.webp`,
@@ -17,15 +27,12 @@ export default async function generateRssFeed(posts) {
 
 	const feed = new RSS(feedOptions);
 
-	posts.map(post =>
-		feed.item({
-			title: post,
-			description: `<img src="${post}" alt="${post} />"`,
-			url: `${site_url}/comic/en/${post}`,
-			date: post.date,
-		}),
-	);
+	posts.map(post => feed.item({
+		title: getPostName(post),
+		description: `<img src="${site_url}/comic/it/${post}" alt="${post} />"`,
+		url: getPageLink({pages: [post], index: 0}),
+		date: post.date,
+	}));
 
-	console.log({feed});
 	fs.writeFileSync('./public/rss.xml', feed.xml({indent: true}));
 }
