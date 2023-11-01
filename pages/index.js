7f91ed4a686b66019ed02180544c7902dc26de51
Motocarota <motocarota@gmail.com>
2023-11-01 11:08:05 +0100

    translate index

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,38 @@ import getChapterLink from '@/utils/getChapterLink';
 import extractValues from '@/utils/extractValues';
 import {getAllPages} from '@/api/comics';
 
+const pageContent = {
+	it: {
+		intro: (
+			<>
+				<Title m='md'>Benvenuto a bordo!</Title>
+				<Text>Cerchi una storia di antiche leggende, eroiche gesta e nobili eroi senza macchia che salvano il mondo dal male?</Text>
+				<Text>Beh, allora sei capitato nel fumetto sbagliato, dolcezza.</Text>
+				<Space m='md'/>
+				<Text>Questa e&apos; la storia di un manipolo di avventurieri brutti, puzzolenti, cattivi ...e anche un po&apos; stronzi.</Text>
+				<Text>Non dire di non essere stato avvisato...</Text>
+			</>
+		),
+	},
+	en: {
+		intro: (
+			<>
+				<Title m='md'>Welcome aboard!</Title>
+				<Text>Looking for heroic deeds, romance and epic battles against evil? EW!</Text>
+				<Text>Just skip on the next webcomic, then!<br/> This is the story of the typical hobo-murderer wrecking ball group that feasts on dungeon master's tears!</Text>
+				<Space m='md'/>
+				<Text>Hope you enjoy it!</Text>
+			</>
+		),
+	},
+};
+
 export default function Home({chapters, locale}) {
+	const t = pageContent[locale];
+
 	return (
 		<>
-			<Title m='md'>Benvenuto a bordo!</Title>
-			<Text>Cerchi una storia di antiche leggende, eroiche gesta e nobili eroi senza macchia che salvano il mondo dal male?</Text>
-			<Text>Beh, allora sei capitato nel fumetto sbagliato, dolcezza.</Text>
-			<Space m='md'/>
-			<Text>Questa e&apos; la storia di un manipolo di avventurieri brutti, puzzolenti, cattivi ...e anche un po&apos; stronzi.</Text>
-			<Text>Non dire di non essere stato avvisato...</Text>
+			{t.intro}
 			<Space m='md' />
 			<Group justify='center' maw={660} m={'auto'}>
 				{chapters.map(({image, url}, index) => (
