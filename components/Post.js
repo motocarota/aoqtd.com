821ea3225f638e7a1fdf7e2c49a472a336c22eab
Motocarota <motocarota@gmail.com>
2024-01-01 13:21:28 +0100

    nicer post message
    Former-commit-id: bf00e6312c368345bbb071bdf210c7b984b69c69 [formerly c70b1a8da0d20b3f616baa1f07991173d5a0abe4]
    Former-commit-id: 5164d16d1dc82fe56ed8f04860db3f7bd37db2f0

--- components/Post.js
+++ components/Post.js
@@ -1,8 +1,17 @@
 import {Box} from '@mantine/core';
 
 export default function Post({md}) {
+	if (!md) {
+		return null;
+	}
+
 	return (
-		<Box maw='700' m='auto'>
+		<Box maw='700' m='auto' style={{
+			background: '#fafafa',
+			padding: '1rem',
+			margin: '1rem',
+			borderRadius: '1rem',
+		}}>
 			<div dangerouslySetInnerHTML={{__html: md}} />
 		</Box>
 	);
