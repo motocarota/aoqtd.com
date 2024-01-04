cc82fb796c101d212025e883c827c6895497389a
Motocarota <motocarota@gmail.com>
2024-01-04 10:47:22 +0100

    suppress next.js warn

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,16 @@ import '@/styles/globals.css';
 import Layout from '@/components/Layout';
 import '@mantine/core/styles.css';
 import {MantineProvider, createTheme} from '@mantine/core';
+import React from 'react';
 
 const theme = createTheme({
 	/** Put your mantine theme override here */
 	primaryColor: 'orange',
 });
+
+if (typeof window === 'undefined') {
+	React.useLayoutEffect = () => {};
+}
 
 export default function App({Component, pageProps}) {
 	return (
