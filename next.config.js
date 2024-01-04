7a07296453a5509c75f91e79fe2e596d3b4a9ae1
Motocarota <motocarota@gmail.com>
2024-01-04 19:17:33 +0100

    adds yaml parser

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const withYaml = require('next-plugin-yaml');
+
 const nextConfig = {
 	output: 'standalone',
 	reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+module.exports = withYaml(nextConfig);
