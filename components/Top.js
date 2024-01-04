c3312786de18ea3b3af20e8d6e973527d8741d60
Motocarota <motocarota@gmail.com>
2024-01-04 17:33:45 +0100

    improved lang swticher

--- components/Top.js
+++ components/Top.js
@@ -1,8 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import {Button, Group} from '@mantine/core';
+import {Box, Button, Group} from '@mantine/core';
 import LocaleSwitcher from './LocaleSwitcher';
-import {useRouter} from 'next/router';
+import Links from './Links';
 
 const links = [
 	{to: '01/000', label: 'Comic'},
@@ -10,32 +10,22 @@ const links = [
 	{to: 'monsters', label: 'Monsters'},
 	{href: 'https://motocarota.art', label: 'Author'},
 	{href: 'https://www.patreon.com/motocarota', label: 'Patreon'},
-	{to: 'faq', label: 'FAQ'},
+	{to: 'about', label: 'About'},
 ];
 
-const Top = () => {
-	const router = useRouter();
-	const setPath = path => {
-		const current = router.asPath;
-		const lang = current.slice(0, 4);
-		router.push(`/${lang}/${path}`);
-	};
-
-	return (
-		<Group justify='center'>
-			<Link href='/'>
-				<Image src='/aoqtd-logo-sm.webp' height={180} width={240} alt='www.aoqtd.com logo' priority className='interactive'/>
-			</Link>
-			<Button.Group>
+const Top = () => (
+	<Group justify='center'>
+		<Link href='/'>
+			<Image src='/aoqtd-logo-sm.webp' height={180} width={240} alt='www.aoqtd.com logo' priority className='interactive'/>
+		</Link>
+		<Button.Group>
+			<Links links={links} />
+			<Box mx='md' my='xs'>
 				<LocaleSwitcher />
-				{links.map(({label, to, href}) => (
-					(to)
-						? <Button key={label} variant='subtle' onClick={() => setPath(to)}>{label}</Button>
-						: <Button key={label} variant='subtle' component='a' href={`${href}`} target='_blank' rel='noreferrer'>{label}</Button>
-				))}
-			</Button.Group>
-		</Group>
-	);
-};
+
+			</Box>
+		</Button.Group>
+	</Group>
+);
 
 export default Top;
