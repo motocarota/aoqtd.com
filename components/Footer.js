a1d8d852006bb98a24af10a6d1298cc524df13cc
Motocarota <motocarota@gmail.com>
2024-01-04 17:36:08 +0100

    nicer footer
    Former-commit-id: 6a63829c35c2cc63c7baea8627a3dc0c1f7d747f [formerly d3cda167111bbb13c6c494819d3cdc66b7ba190c]
    Former-commit-id: ec06349740b606ab90c4bddff2a03529f7278d03

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,39 @@
-import {Anchor, Center, Stack} from '@mantine/core';
+import {Anchor, Center, Group, Space, Stack} from '@mantine/core';
+import Image from 'next/image';
+import Links from './Links';
+
+const linksCol1 = [
+	{label: 'About', to: 'faq'},
+	{label: 'Characters', to: 'characters'},
+	{label: 'Monsters', to: 'monsters'},
+	// {label: 'PNGs', to: 'pngs'},
+	{label: 'Author', href: 'https://motocarota.art'},
+];
+
+const linksCol2 = [
+	{label: 'Hall of Fame', to: 'hall-of-fame'},
+	{label: 'Other webcomics', to: 'other-webcomics'},
+	{label: 'Fan art', to: 'fan-art'},
+	{label: 'd20 SRD', href: 'https://www.d20srd.org/'},
+];
+
+const linksCol3 = [
+	{label: 'Join the party', href: 'https://www.patreon.com/motocarota'},
+	{label: 'Report a typo', href: 'mailto:motocarota@gmail.com'},
+	{label: 'Share to a friend', to: 'share'},
+	{label: 'Send a suggestion', href: 'mailto:motocarota@gmail.com'},
+];
 
 const Footer = () => (
 	<Center my='lg' mx='auto'>
 		<Stack>
+			<Group wrap='wrap'>
+				<Links links={linksCol1} isVertical />
+				<Links links={linksCol2} isVertical />
+				<Links links={linksCol3} isVertical />
+				<Space w={150}/>
+				<Image src='/beholder.webp' width={150} height={188} alt='beholder'/>
+			</Group>
 			<small>Copyright ©2003-{new Date().getFullYear()} <Anchor href='https://motocarota.art' target='_blank' rel='noreferrer'>Simone Poggi</Anchor><br/>All Rights Reserved</small>
 		</Stack>
 	</Center>
